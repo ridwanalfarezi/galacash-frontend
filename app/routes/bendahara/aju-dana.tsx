c273c43004c66f93057d43b5318cbdd0681799fa
ridwanalfarezi <donnysutiandy@gmail.com>
2026-01-16 03:21:11 +0700

    fix: remove HydrateFallback from non-root routes (SPA mode incompatible)

--- app/routes/bendahara/aju-dana.tsx
+++ app/routes/bendahara/aju-dana.tsx
@@ -31,14 +31,6 @@ export async function clientLoader() {
 
 clientLoader.hydrate = true
 
-export function HydrateFallback() {
-  return (
-    <div className="flex justify-center py-12">
-      <div className="h-12 w-12 animate-spin rounded-full border-4 border-gray-300 border-t-blue-500"></div>
-    </div>
-  )
-}
-
 export default function AjuDana({ loaderData }: Route.ComponentProps) {
   return (
     <HydrationBoundary state={loaderData.dehydratedState}>
